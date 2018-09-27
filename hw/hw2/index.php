5433b777889b7fde1582f59b5a65a26b66cc1b1a
Kye <ksolomon@csumb.edu>
2018-09-27 17:27:03 +0000

    lab 3

---
+++ hw/hw2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Random Song Generator </title>
+        <meta-charset = "utf-8" />
+        <link rel="stylesheet" href="styles.css" type="text/css" />
+        <h1>Random Song Generator</h1>
+        <button>Shuffle!</button>
+    </head>
+    <body>
+        <?php
+            include 'functions.php';
+        ?>
+
+    </body>
+</html>
