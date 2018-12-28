10f4c60dc2984b2c70f9f94b3723e61a70c89c03
Kye <ksolomon@csumb.edu>
2018-12-28 00:39:42 +0000

    lab 5 troubleshoot

--- labs/lab5/index.php
+++ labs/lab5/index.php
@@ -57,7 +57,8 @@ function displayRandomQuote() {
     <head>
         <title> Lab 5: Random Famous Quote </title>
         <style>@import 'styles.css';</style>
-        
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     </head>
     <body>
 
